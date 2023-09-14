99e71f78794dcada9f185ef21f73d5e3362de9a9
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-14 19:31:08 +0330

    Add Header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,20 @@
+import "./globals.css";
 import vazirFont from "@/constants/localFonts";
+
+import Header from "./Header";
 
 export const metadata = {
   title: "Shop Panel",
   description: "Next Shop Panel",
 };
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="fa">
-      <body className={`${vazirFont.variable} font-sans`}>{children}</body>
+    <html lang="fa" dir="rtl">
+      <body className={`${vazirFont.variable} font-sans`}>
+        <Header />
+        <div className="container xl:max-w-screen-xl">{children}</div>
+      </body>
     </html>
   );
 }
