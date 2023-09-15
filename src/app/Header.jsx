f69552a534cd6c0cbfd1434dafed2f6636b050ed
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-15 18:06:53 +0330

    Set condition for user login button

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Link from "next/link";
 
+import { useGetUser } from "@/hooks/useAuth";
+
 function Header() {
+  const { data, error, isLoading } = useGetUser();
+  const { user, cart } = data || {};
+
   return (
-    <header className="shadow-md mb-10">
+    <header
+      className={`sticky top-0 mb-10 shadow-md transition-all duration-200 ${
+        isLoading ? "opacity-70 blur-sm" : "opacity-100 blur-0"
+      }`}>
       <nav>
-        <ul className="flex items-center justify-between py-2 container xl:max-w-screen-xl">
+        <ul className="xl:max-w-screen-xl container flex items-center justify-between py-2">
           <li>
             <Link className="block py-2" href="/">
               خانه
@@ -16,10 +26,19 @@ function Header() {
             </Link>
           </li>
           <li>
-            <Link className="block py-2" href="/auth">
-              ورود
+            <Link className="block py-2" href="/cart">
+              سبد خرید - ({cart ? cart?.payDetail?.productIds.length : 0})
             </Link>
           </li>
+          {user ? (
+            <span>{user?.name}</span>
+          ) : (
+            <li>
+              <Link className="block py-2" href="/auth">
+                ورود
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
