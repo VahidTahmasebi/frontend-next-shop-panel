55d91e9707870d023f6ca3261ad25dfe98ecac8a
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-16 14:22:12 +0330

    Optimize user access syntax

--- src/middleware.js
+++ src/middleware.js
@@ -1,31 +1,21 @@
 import { NextResponse } from "next/server";
 
+import middlewareAuth from "./utils/middlewareAuth";
+
 export async function middleware(req) {
   const url = req.url;
   const pathname = req.nextUrl.pathname;
 
   if (pathname.startsWith("/profile")) {
-    let strCookie = "";
-    req.cookies.getAll().forEach((item) => {
-      strCookie += `${item?.name}=${item?.value}; `;
-    });
-
-    const { data } = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/user/profile`,
-      {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Cookies: strCookie,
-        },
-      }
-    ).then((res) => res.json());
-    const { user } = data || {};
+    const user = await middlewareAuth(req);
     if (!user) return NextResponse.redirect(new URL("/auth", req.url));
   }
 
-  if (pathname.startsWith("/amdin")) {
-    console.log("this is admin req!!");
+  if (pathname.startsWith("/admin")) {
+    const user = await middlewareAuth(req);
+    if (!user) return NextResponse.redirect(new URL("/auth", req.url));
+    if (user && user.role !== "ADMIN")
+      return NextResponse.redirect(new URL("/", req.url));
   }
 }
 
