ab57a8df741fd8243374e588dab5eeec1ed15859
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-21 11:11:40 +0330

    Add logout and dashboard bottom

--- src/app/(profile)/profile/SideBar.jsx
+++ src/app/(profile)/profile/SideBar.jsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 
+import { logout } from "@/services/authServices";
+
 function SideBar() {
+  const logoutHandler = async () => {
+    await logout();
+    document.location.href = "/";
+  };
+
   return (
     <div>
       <ul className="flex flex-col space-y-8">
@@ -8,7 +15,13 @@ function SideBar() {
           <Link href="/">صفحه اصلی</Link>
         </li>
         <li>
+          <Link href="/profile">داشبورد</Link>
+        </li>
+        <li>
           <Link href="/profile/me">اطلاعات کاربری</Link>
+        </li>
+        <li>
+          <button onClick={logoutHandler}>خروج از حساب کاربری</button>
         </li>
       </ul>
     </div>
