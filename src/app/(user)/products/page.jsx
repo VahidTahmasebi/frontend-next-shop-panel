30910c04cbdf79078eba5739c56c423028d6aac4
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-27 17:09:24 +0330

    Add category and product section

--- src/app/(user)/products/page.jsx
+++ src/app/(user)/products/page.jsx
@@ -1,5 +1,32 @@
-function Products() {
-  return <div>Products</div>;
+import { getCategories } from "@/services/categoryServices";
+import { getProducts } from "@/services/productServices";
+import CategorySidebar from "./CategorySidebar";
+
+async function Products() {
+  const { categories } = await getCategories();
+  const { products } = await getProducts();
+
+  return (
+    <div>
+      <h1 className="mb-6 text-xl font-bold">صفحه محصولات</h1>
+
+      <div className="grid grid-cols-4">
+        <CategorySidebar categories={categories} />
+
+        <div>
+          {products.map((product) => {
+            return (
+              <div
+                key={product._id}
+                className="col-span-1 p-4 border rounded-xl shadow-md">
+                <h2 className="font-bold">{product.title}</h2>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Products;
