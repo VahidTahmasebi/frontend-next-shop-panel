731f6ad5811ef7371beafa57381a102c4ce7c199
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-29 12:57:25 +0330

    Add page product

---
+++ src/app/(user)/products/[slug]/page.jsx
@@ -0,0 +1,46 @@
+import { getOneProductBySlug, getProducts } from "@/services/productServices";
+
+export const dynamic = "force-static";
+export const dynamicParams = false;
+
+async function page({ params }) {
+  const { slug } = params;
+
+  const { product } = await getOneProductBySlug(slug);
+
+  return (
+    <div>
+      <h1 className="mb-6 text-2xl font-bold">{product.title}</h1>
+      <p className="mb-6">{product.description}</p>
+      <p className="mb-6">
+        قیمت محصول :{" "}
+        <span className={`${product.discount ? "line-through" : "font-bold"}`}>
+          {product.price}
+        </span>
+      </p>
+      {!!product.discount && (
+        <div className="flex items-center gap-x-2 mb-6">
+          <p className="text-xl font-bold">
+            قیمت با تخفیف : {product.offPrice}
+          </p>
+          <div className="py-0.5 px-2 rounded-xl text-sm text-white bg-rose-500">
+            {product.discount} %
+          </div>
+        </div>
+      )}
+      <div>
+        <button className="btn btn--primary">اضافه کردن به سبد خرید</button>
+      </div>
+    </div>
+  );
+}
+
+export default page;
+
+export async function generateStaticParams() {
+  const { products } = await getProducts();
+
+  return products.map((product) => ({
+    slug: product.slug,
+  }));
+}
