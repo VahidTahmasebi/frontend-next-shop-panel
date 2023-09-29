cc7578151b377f9a6b40c4fa2d5101966e9384c1
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-29 22:52:40 +0330

    Add payment create syntax

--- src/app/(user)/cart/CartSummary.jsx
+++ src/app/(user)/cart/CartSummary.jsx
@@ -1,7 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
 import { toPersianNumbersWithComma } from "@/utils/toPersianNumber";
+import Loading from "@/common/Loading";
+
+import { createPayment } from "@/services/paymentService";
 
 function CartSummary({ payDetail }) {
   const { totalOffAmount, totalPrice, totalGrossPrice } = payDetail;
+
+  const { isLoading, mutateAsync } = useMutation({ mutationFn: createPayment });
+
+  const queryClient = useQueryClient();
+
+  const createPaymentHandler = async () => {
+    try {
+      const { message } = await mutateAsync();
+      toast.success(message);
+      queryClient.invalidateQueries({ queryKey: ["get-user"] });
+    } catch (error) {
+      if (error?.response?.data) {
+        toast.error(
+          error?.response?.data?.message
+            ? error?.response?.data?.message
+            : "خطایی رخ داد"
+        );
+      }
+    }
+  };
 
   return (
     <div className="p-2 border rounded-xl">
@@ -18,7 +44,17 @@ function CartSummary({ payDetail }) {
         <span>مبلغ قابل پرداخت</span>
         <span>{toPersianNumbersWithComma(totalPrice)}</span>
       </div>
-      <button className="w-full btn btn--primary">ثبت سفارش</button>
+      <div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <button
+            className="w-full btn btn--primary"
+            onClick={createPaymentHandler}>
+            ثبت سفارش
+          </button>
+        )}
+      </div>
     </div>
   );
 }
