a40d6f9bd668b435c10914c7e0d9019fcd103f17
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-09-30 11:15:09 +0330

    Add a table of the users last orders

--- src/app/(profile)/profile/page.jsx
+++ src/app/(profile)/profile/page.jsx
@@ -1,24 +1,41 @@
 "use client";
 
+import Link from "next/link";
+
 import { useGetUser } from "@/hooks/useAuth";
 
 import { toLocalDateString } from "@/utils/toLocalDate";
 import Loading from "@/common/Loading";
 
+import PaymentTable from "./payments/paymentTable";
+
 function Profile() {
   const { data, isLoading } = useGetUser();
-  const { user } = data || {};
+  const { user, payments } = data || {};
 
   if (isLoading) return <Loading />;
 
   return (
     <div className="py-4">
       <h1 className="mb-4 text-xl">{user.name} خوش آمدی</h1>
-      <h1 className="font-bold">
+      <p className="font-bold">
         <span className="font-bold">
           تاریخ پیوستن: {toLocalDateString(user.createdAt)}
         </span>
-      </h1>
+      </p>
+      <div className="mt-8 p-4 border rounded-xl">
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold">آخرین سفارشات کاربر</h2>
+          <Link href="/profile/payments" className="text-primary-900">
+            مشاهده همه سفارشات
+          </Link>
+        </div>
+        <PaymentTable
+          payments={payments
+            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            .slice(0, 3)}
+        />
+      </div>
     </div>
   );
 }
